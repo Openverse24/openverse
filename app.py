11d3ed3bcfe596ef092dd4a7ba6e1a119569056c
Moritzjenny <moritzjenny@hotmail.com>
2023-11-30 18:38:39 +0100

    mute set get

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Example endpoint to toggle mute status
+mute_status = False
+
+@app.route('/mute-state', methods=['GET'])
+def get_mute_state():
+    return jsonify({'mute_status': mute_status})
+
+@app.route('/toggle-mute', methods=['POST'])
+def toggle_mute():
+    global mute_status
+    mute_status = not mute_status
+    return jsonify({'mute_status': mute_status})
+
 @app.route("/")
 def index():
     return "Hello World!"
