37f756b65d56230982c0c761ad02f86341afa57d
Moritzjenny <moritzjenny@hotmail.com>
2023-11-30 18:55:02 +0100

    atomicity

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from threading import Lock
 
 app = Flask(__name__)
 
 # Example endpoint to toggle mute status
 mute_status = False
+mute_status_lock = Lock()
 
 @app.route('/mute-state', methods=['GET'])
 def get_mute_state():
-    return jsonify({'mute_status': mute_status})
+    with mute_status_lock:
+        return jsonify({'mute_status': mute_status})
 
 @app.route('/toggle-mute', methods=['POST'])
 def toggle_mute():
     global mute_status
-    mute_status = not mute_status
-    return jsonify({'mute_status': mute_status})
+    with mute_status_lock:
+        mute_status = not mute_status
+        return jsonify({'mute_status': mute_status})
 
 @app.route("/")
 def index():
