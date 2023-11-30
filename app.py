677d28710bd2344837f41b7476f1409cc031789c
Moritzjenny <moritzjenny@hotmail.com>
2023-11-30 18:58:46 +0100

    new technique

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask, jsonify
-from threading import Lock
+from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
-# Example endpoint to toggle mute status
-mute_status = False
-mute_status_lock = Lock()
+# File path to store mute status
+mute_status_file = 'mute_status.txt'
 
+# Function to read mute status from the file
+def read_mute_status():
+    try:
+        with open(mute_status_file, 'r') as file:
+            return file.read().strip().lower() == 'true'
+    except FileNotFoundError:
+        return False
+
+# Function to write mute status to the file
+def write_mute_status(status):
+    with open(mute_status_file, 'w') as file:
+        file.write('true' if status else 'false')
+
+# Example endpoint to toggle mute status
 @app.route('/mute-state', methods=['GET'])
 def get_mute_state():
-    with mute_status_lock:
-        return jsonify({'mute_status': mute_status})
+    mute_status = read_mute_status()
+    return jsonify({'mute_status': mute_status})
 
 @app.route('/toggle-mute', methods=['POST'])
 def toggle_mute():
-    global mute_status
-    with mute_status_lock:
-        mute_status = not mute_status
-        return jsonify({'mute_status': mute_status})
+    mute_status = not read_mute_status()
+    write_mute_status(mute_status)
+    return jsonify({'mute_status': mute_status})
 
 @app.route("/")
 def index():
     return "Hello World!"
+
