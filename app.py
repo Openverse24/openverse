40ac04d7b794f02f3ebbd908727195ce9d0f1047
Moritzjenny <moritzjenny@hotmail.com>
2023-12-04 14:22:50 +0100

    add introducition functionality

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ mute_status_file = 'mute_status.txt'
 # File path to store last clicked button and introduction text
 last_clicked_file = 'last_clicked.txt'
 
+# File path to store the introduction counter
+introduction_counter_file = 'introduction.txt'
+
 # Function to read mute status from the file
 def read_mute_status():
     try:
@@ -42,6 +45,19 @@ def write_last_clicked(option, introduction_text):
     with open(last_clicked_file, 'w') as file:
         file.write(json.dumps(data))
 
+# Function to read the introduction counter file
+def read_introduction():
+    try:
+        with open(introduction_counter_file, 'r') as file:
+            return int(file.read().strip())
+    except FileNotFoundError:
+        return 0
+
+# Function to write the introduction counter file
+def write_introduction(counter):
+    with open(introduction_counter_file, 'w') as file:
+        file.write(str(counter))
+
 # Example endpoint to toggle mute status
 @app.route('/mute-state', methods=['GET'])
 def get_mute_state():
@@ -53,6 +69,14 @@ def get_mute_state():
 def get_option():
     option_data = json.loads(read_last_clicked())
     return jsonify(option_data)
+
+# New endpoint to get the current counter value
+@app.route('/get-introduction-count', methods=['GET'])
+def get_introduction_count():
+    # Read the current counter value
+    current_counter = read_introduction()
+
+    return jsonify({'counter': current_counter})
 
 # Example endpoint to toggle mute status
 @app.route('/toggle-mute', methods=['POST'])
@@ -79,6 +103,21 @@ def set_option():
     write_last_clicked(option, introduction_text)
 
     return jsonify({'success': True, 'last_clicked': option, 'introduction_text': introduction_text})
+
+
+# New endpoint to increment the counter
+@app.route('/introduction', methods=['POST'])
+def introduction():
+    # Read the current counter value
+    current_counter = read_introduction()
+
+    # Increment the counter
+    new_counter = current_counter + 1
+
+    # Write the new counter value to the file
+    write_introduction(new_counter)
+
+    return jsonify({'counter': new_counter})
 
 @app.route("/")
 def index():
